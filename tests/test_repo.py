ead8de9a1554091829b0f0fc8b5fb952a1e2afbd
Chetan Kini <chetan@superconductive.com>
2022-08-26 13:17:12 -0400

    test: ensure lack of env var doesn't raise

--- tests/test_repo.py
+++ tests/test_repo.py
@@ -2,16 +2,18 @@ import pytest
 
 from qn.repo import Repo
 
+
 def test_repo_create_no_root_raises_error(monkeypatch):
-    monkeypatch.delenv("QN_ROOT")
+    monkeypatch.delenv("QN_ROOT", raising=False)
 
     with pytest.raises(ValueError) as e:
         Repo.create()
 
     assert "No value found for QN_ROOT env var" == str(e.value)
 
+
 def test_repo_create_nonexistent_root_raises_error(monkeypatch):
-    path =  "this/does/not/exist"
+    path = "this/does/not/exist"
     monkeypatch.setenv("QN_ROOT", path)
 
     with pytest.raises(ValueError) as e:
@@ -19,8 +21,9 @@ def test_repo_create_nonexistent_root_raises_error(monkeypatch):
 
     assert f"QN_ROOT '{path}' does not exist" == str(e.value)
 
+
 def test_repo_create_file_root_raises_error(monkeypatch, tmp_path) -> None:
-    path =  tmp_path / "root.md"
+    path = tmp_path / "root.md"
     path.touch()
     monkeypatch.setenv("QN_ROOT", str(path))
 
