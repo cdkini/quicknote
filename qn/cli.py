0bb3fb54447822cdbe01b350f0f76691cf7fa430
Chetan Kini <chetan@superconductive.com>
2022-08-26 11:52:36 -0400

    feat: add CLI method stubs

--- qn/cli.py
+++ qn/cli.py
@@ -9,5 +9,35 @@ def cli(ctx: click.Context) -> None:
     """
 
 
+@cli.command("add")
+@click.pass_obj
+def add_cmd() -> None:
+    pass
+
+
+@cli.command("open")
+@click.pass_obj
+def open_cmd() -> None:
+    pass
+
+
+@cli.command("grep")
+@click.pass_obj
+def grep_cmd() -> None:
+    pass
+
+
+@cli.command("ls")
+@click.pass_obj
+def ls_cmd() -> None:
+    pass
+
+
+@cli.command("rm")
+@click.pass_obj
+def rm_cmd() -> None:
+    pass
+
+
 if __name__ == "__main__":
     cli()
