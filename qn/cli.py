4d189e939a715710893c0bd08b07365d977ff800
Chetan Kini <chetan@superconductive.com>
2022-09-04 11:36:51 -0400

    chore: add docstrings to CLI commands

--- qn/cli.py
+++ qn/cli.py
@@ -19,6 +19,9 @@ def cli(ctx: click.Context) -> None:
 @cli.group(name="note")
 @click.pass_context
 def note(ctx: click.Context):
+    """
+    Manage CRUD around notes.
+    """
     pass
 
 
@@ -27,6 +30,9 @@ def note(ctx: click.Context):
 @click.argument("name", nargs=1)
 @click.option("--template", "-t", default=None)
 def add_note(repo: Repo, name: str, template: Optional[str]) -> None:
+    """
+    Create a new note.
+    """
     repo.add_note(name, template)
 
 
@@ -34,12 +40,18 @@ def add_note(repo: Repo, name: str, template: Optional[str]) -> None:
 @click.pass_obj
 @click.argument("names", nargs=-1)
 def open_note(repo: Repo, names: Tuple[str]) -> None:
+    """
+    Open an existing note.
+    """
     repo.open_notes(names)
 
 
 @note.command("ls")
 @click.pass_obj
 def ls_note(repo: Repo) -> None:
+    """
+    List notes.
+    """
     notes = repo.list_notes()
     for note in notes:
         click.echo(note)
@@ -49,18 +61,27 @@ def ls_note(repo: Repo) -> None:
 @click.pass_obj
 @click.argument("names", nargs=-1)
 def rm_note(repo: Repo, names: Tuple[str]) -> None:
+    """
+    Delete notes.
+    """
     repo.delete_notes(names)
 
 
 @note.command("daily")
 @click.pass_obj
 def daily_note(repo: Repo) -> None:
+    """
+    Open daily note.
+    """
     repo.open_daily_note()
 
 
 @cli.group(name="tmpl")
 @click.pass_context
 def tmpl(ctx: click.Context):
+    """
+    Manage CRUD around templates.
+    """
     pass
 
 
@@ -68,6 +89,9 @@ def tmpl(ctx: click.Context):
 @click.pass_obj
 @click.argument("name", nargs=1)
 def add_tmpl(repo: Repo, name: str) -> None:
+    """
+    Create a new temlate.
+    """
     repo.add_template(name)
 
 
@@ -75,12 +99,18 @@ def add_tmpl(repo: Repo, name: str) -> None:
 @click.pass_obj
 @click.argument("names", nargs=-1)
 def open_tmpl(repo: Repo, names: Tuple[str]) -> None:
+    """
+    Open an existing template.
+    """
     repo.open_templates(names)
 
 
 @tmpl.command("ls")
 @click.pass_obj
 def ls_tmpl(repo: Repo) -> None:
+    """
+    List templates.
+    """
     templates = repo.list_templates()
     for template in templates:
         click.echo(template)
@@ -90,6 +120,9 @@ def ls_tmpl(repo: Repo) -> None:
 @click.pass_obj
 @click.argument("names", nargs=-1)
 def rm_tmpl(repo: Repo, names: Tuple[str]) -> None:
+    """
+    Delete templates.
+    """
     repo.delete_templates(names)
 
 
@@ -99,6 +132,9 @@ def rm_tmpl(repo: Repo, names: Tuple[str]) -> None:
 @click.pass_obj
 @click.argument("args", nargs=-1)
 def grep_cmd(repo: Repo, args: Tuple[str, ...]) -> None:
+    """
+    Use ripgrep through parse through notes and templates.
+    """
     repo.grep_notes(args)
 
 
